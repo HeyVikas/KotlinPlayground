6b65a04540915d0ea3472cb6950ceb454e7815fc
HeyVikas <namastelalwani@gmail.com>
2023-12-06 00:01:23 +0530

    Conditional Statement

---
+++ src/main/kotlin/ProblemsOnConditionalStatements.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val x = readln().toInt()
+
+    if (x < 70){
+        println(("Order Confirmed"))
+    }
+    else{
+        println("Order Limit reached")
+    }
+    println("Thank You")
+}
